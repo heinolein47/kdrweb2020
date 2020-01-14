2629cecd3b556a74bab8d75e402c6aa7fb831f70
KDR on Javabox <javabox@kevinrollins.com>
2020-01-14 14:25:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Container, Row, Col,Jumbotron,Card, CardSubtitle, CardText} from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col xs="3"><p>Sidebar</p>
+            <li>Reading Lists</li>
+            <li>Marginal Improvements</li>
+          </Col>
+          <Col xs="6">
+             <Jumbotron>
+               <h1>learning, consulting, writing</h1>
+             </Jumbotron>   
+             <Card>Bottom Up Design
+               <CardSubtitle>One useful piece at a time</CardSubtitle>
+               <CardText>
+                By starting with a simple skeleton and adding pieces as they seem useful and relevant, it is possible to come up with a design without having to think of the exact right plan at the very beginning.
+               </CardText>
+
+             </Card>
+           </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
